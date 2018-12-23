28ed1c2668b023f7790a0f51015007191893a0ad
Goufan A Zock <Greko2017@users.noreply.github.com>
2018-12-24 00:40:03 +0100

    Initial Commit

--- jb_app/notifications/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from jb_app.notifications.models import Notification
-
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('recipient', 'actor', 'verb', 'unread', )
-    list_filter = ('recipient', 'unread', )
