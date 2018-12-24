91a6d40dd719afab781c13226368fa5cf8916332
Goufan A Zock <Greko2017@users.noreply.github.com>
2018-12-24 04:45:02 +0100

    Initial Commit

--- blog/settings.py
+++ blog/settings.py
@@ -222,3 +222,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+WHITENOISE_STATIC_PREFIX = '/static/'
