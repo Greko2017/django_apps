cfeb353ca2bec739c1464e8e4cdef026bdba2236
Goufan A Zock <Greko2017@users.noreply.github.com>
2018-12-24 03:10:21 +0100

    Initial Commit

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -7,15 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os, settings
+import os
 
 from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
-application.add_files(os.path.join(BASE_DIR), "static")
+application = WhiteNoise(application, root=STATIC_ROOT)
+application.add_files(os.path.join(BASE_DIR), "static") 
+s
